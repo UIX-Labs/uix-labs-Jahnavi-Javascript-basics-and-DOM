6db493eb3beeb09436d796c1fe85d612ac11acc2
Jahnavi <kjahnavisaipriya@gmail.com>
2024-03-17 12:59:32 +0530

    task: Implementing JS Objects

--- Task_objects/index.js
+++ Task_objects/index.js
@@ -8,7 +8,9 @@ const cat = {
 };
 
 // Write the code for 1st task below
-
+let catName = cat['name'];
+cat.greeting;
+cat.color = 'Black';
 
 //Write your code above
 
@@ -16,9 +18,25 @@ const cat = {
 
 
 //Write your 2nd task below this
+const bands = {
+    name  : 'Black Pink',
+    nationality : 'Korean',
+    genre : 'action',
+    members : 10,
+    formed : 2020,
+    split: false,
+    albums : [
+        {
+            name : 'album 1',
+            released : 2020
+        },{
+            name : 'album 2',
+            released : 2018
+        }
+    ]
+}
 
-
-
+console.log(cat,bands)
 //Write your code above
 
 
